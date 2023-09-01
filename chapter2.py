48219b2e065ebdd5b4206cb1e39d73ed40bae078
mafarl <marie.v.frolova@gmail.com>
2023-09-01 16:30:35 +0300

    2E

--- chapter2.py
+++ chapter2.py
@@ -260,14 +260,17 @@ def score(motifs):
     return len(motifs[0])*len(motifs) - max_count
 
 
-# This one is the same
+# For the Subtle Motif problem returns score(motifs) = 70, while in book - score(motifs) = 58
+# => need to improve
 def greedy_motif_search(dna, k, t):
     # Initialise best score as high as possible
     import math
     best_score = math.inf
     # Collection of motifs from each string of dna
     best_motifs = []
+
     for string in dna:
+        dna = [a.upper() for a in dna]
         best_motifs.append(string[:k])
 
     for i in range(len(dna[0]) - k + 1):
@@ -292,7 +295,91 @@ def greedy_motif_search(dna, k, t):
     return best_motifs
 
 
-print(greedy_motif_search(["TTACCTTAAC", "GATGTCTGTC",
-                           "ACGGCGTTAG", "CCCTAACGAG",
-                           "CGTCAGAGGT"], 4, 1))
+# Laplace’s Rule of Succession - substituting zero-probabilities with pseudocounts
+# (adds 1 to each element of COUNT(Motifs)
+def laplaces_rule(motifs, number):
+    profile_matrix = [[] for _ in range(4)]
 
+    for i in range(len(motifs[0])):
+
+        current_column = {"A": 0,
+                          "C": 0,
+                          "G": 0,
+                          "T": 0}
+
+        for motif in motifs:
+            current_column[motif[i]] += 1
+        counter = 0
+        for key, value in current_column.items():
+            profile_matrix[nucleotides.index(key)].append((value + 1) / (4 + number))
+            counter += 1
+    return profile_matrix
+
+
+# Gives score(motifs) = 39, while 41 in book
+def greedy_motif_search_with_pseudocounts(dna, k, t):
+    # Initialise best score as high as possible
+    import math
+    best_score = math.inf
+    # Collection of motifs from each string of dna
+    best_motifs = []
+
+    for string in dna:
+        dna = [a.upper() for a in dna]
+        best_motifs.append(string[:k])
+
+    number = 0
+
+    for i in range(len(dna[0]) - k + 1):
+        # Array with current motifs for selected k-mer in dna[0]
+        # print(dna[0][i:(i + k)])
+        motifs = [dna[0][i:(i + k)]]
+
+        for j in range(1, t):
+
+            # Changing this part - instead of building profile matrix, use Laplace's rule to build a profile matrix
+            profile_matrix = laplaces_rule(motifs, number)
+            number += 1
+            current_motif = profile_most_probable_k_mer(dna[j], k, profile_matrix)
+            motifs.append(current_motif)
+
+        if score(motifs) < best_score:
+            best_score = score(motifs)
+            best_motifs = motifs
+
+    return best_motifs
+
+
+"""
+    This part of code test how accurate greedy_motif_search is
+    For the Subtle Motif problem returns score(motifs) = 70, while in book - score(motifs) = 58
+    => need to improve
+    
+output = greedy_motif_search_with_pseudocounts(["atgaccgggatactgatAgAAgAAAGGttGGGggcgtacacattagataaacgtatgaagtacgttagactcggcgccgccg",
+                           "acccctattttttgagcagatttagtgacctggaaaaaaaatttgagtacaaaacttttccgaatacAAtAAAAcGGcGGGa",
+                           "tgagtatccctgggatgacttAAAAtAAtGGaGtGGtgctctcccgatttttgaatatgtaggatcattcgccagggtccga",
+                           "gctgagaattggatgcAAAAAAAGGGattGtccacgcaatcgcgaaccaacgcggacccaaaggcaagaccgataaaggaga",
+                           "tcccttttgcggtaatgtgccgggaggctggttacgtagggaagccctaacggacttaatAtAAtAAAGGaaGGGcttatag",
+                           "gtcaatcatgttcttgtgaatggatttAAcAAtAAGGGctGGgaccgcttggcgcacccaaattcagtgtgggcgagcgcaa",
+                           "cggttttggcccttgttagaggcccccgtAtAAAcAAGGaGGGccaattatgagagagctaatctatcgcgtgcgtgttcat",
+                           "aacttgagttAAAAAAtAGGGaGccctggggcacatacaagaggagtcttccttatcagttaatgctgtatgacactatgta",
+                           "ttggcccattggctaaaagcccaacttgacaaatggaagatagaatccttgcatActAAAAAGGaGcGGaccgaaagggaag",
+                           "ctggtgagcaacgacagattcttacgtgcattagctcgcttccggggatctaatagcacgaagcttActAAAAAGGaGcGGa"], 15, 10)
+
+print(score(output))
+give = []
+this = [''.join(seq) for seq in zip(*output)]
+for i in range(len(this)):
+    current_column_dict = {"A": 0,
+                           "C": 0,
+                           "G": 0,
+                           "T": 0}
+    for j in range(len(this[0])):
+        current_column_dict[this[i][j]] += 1
+    give.append([key for key, value in current_column_dict.items() if value == max(current_column_dict.values())])
+
+result = ""
+for letter in give:
+    result += letter[0]
+    
+"""
